f517b6f0a4b6755a196e8cb088f4a7e18f6d22e7
=Alberto <jlr0013@alu.medac.es>
2025-01-28 13:56:00 +0100

    Comunicación entre fragment con otra plantilla

--- app/src/main/java/com/example/interfaces1/FirstFragment.java
+++ app/src/main/java/com/example/interfaces1/FirstFragment.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.interfaces1.databinding.FragmentFirstBinding;
@@ -16,32 +19,21 @@ public class FirstFragment extends Fragment {
     private FragmentFirstBinding binding;
 
     @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
-
-        binding = FragmentFirstBinding.inflate(inflater, container, false);
-        return binding.getRoot();
-
-    }
-
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Cargar la vista del diseño del fragment
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+        //Referencia al boton que realiza la accion
+        Button buttonToNext = view.findViewById((R.id.button_first));
+        //Configurar la accion del boton
+        buttonToNext.setOnClickListener(v -> {
+            NavController navController = Navigation.findNavController(view);
+            navController.navigate(R.id.action_FirstFragment_to_SecondFragment);
         });
-    }
+        return view;
+    }}
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
 
-}
+
+
+
--- app/src/main/java/com/example/interfaces1/SecondFragment.java
+++ app/src/main/java/com/example/interfaces1/SecondFragment.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.interfaces1.databinding.FragmentSecondBinding;
@@ -16,32 +19,17 @@ public class SecondFragment extends Fragment {
     private FragmentSecondBinding binding;
 
     @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
-
-        binding = FragmentSecondBinding.inflate(inflater, container, false);
-        return binding.getRoot();
-
-    }
-
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
-            }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Cargar la vista del diseño del fragment
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+        //Referencia al boton que realiza la accion
+        Button buttonToNext2 = view.findViewById((R.id.button_second));
+        //Configurar la accion del boton
+        buttonToNext2.setOnClickListener(v -> {
+            NavController navController = Navigation.findNavController(view);
+            navController.navigate(R.id.action_SecondFragment_to_FirstFragment);
         });
-    }
+        return view;
+    }}
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
-
-}
